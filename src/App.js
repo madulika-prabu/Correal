ef82cc0c5baa309cfc8eb9ff34af74b14bc85781
Madulika Prabusankar <131234604+madulika-prabu@users.noreply.github.com>
2024-05-15 14:04:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+<head>
+	<title>Correal Web App</title>
+	<link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+	<header>
+		<div class="logo-container">
+			<img src="C:\Users\TEMP.SCE\Desktop\correal\my-app\src\Add a heading.png" alt="Correal Logo" />
+			<span>Correal</span>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="personaltraining.html">PERSONAL & ONLINE TRAINING</a></li>
+				<li><a href="indexdesktop.html">EBOOKS</a></li>
+				<li><a href="apps.html">APPS</a></li>
+				<li><a href="instagram.html">INSTAGRAM SHOUTOUTS</a></li>
+				<li><a href="contact.html">CONTACT</a></li>
+			</ul>
+		</nav>
+	</header>
+	
+</body>
+
+    </div>
+  );
+}
+
+export default App;
+
